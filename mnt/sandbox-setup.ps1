7520d54374ea03674d3f9b79dc9c0eadced032bf
Harry Merritt <harry@merritt.es>
2021-05-25 23:06:58 +0100

    Add notepad++ to context menu
    Manually update registry for each file

--- mnt/sandbox-setup.ps1
+++ mnt/sandbox-setup.ps1
@@ -11,6 +11,9 @@ scoop bucket add hmerritt "https://github.com/hmerritt/scoop-bucket"
 scoop install notepadplusplus
 scoop install windows-terminal
 
+## Notepad in contextmenu
+Invoke-Command { reg import C:\mnt\bin\notepad-contextmenu.reg }
+
 
 # Launch programs
 ## windows-terminal as an administrator
